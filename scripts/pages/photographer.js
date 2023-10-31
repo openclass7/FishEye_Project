fd0e664eaec097bc46b7ed6ace40b5d69cb33e56
openclass7 <aissat.fateh@gmail.com>
2023-10-31 09:16:16 +0100

    commit 2

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -60,99 +60,84 @@ async function getPhotographers()
  
 
  
+
+   function photographerTemplate_2(mediaData) {
+    const { photographerId, title,image,video,likes } = mediaData;
+
+    console.log(mediaData)
+
+
+function templatePhotographerMedia(){
+    if(image != undefined){
+      html_injected= `<img   class="media_photos" 
+       src="./assets/photographersGallery/${photographerId}/${image}" />`
+    }else if( video != undefined){
+       html_injected=
+          `<video   class="media_videos"  >
+               <source src="./assets/photographersGallery/${photographerId}/${ video}" 
+                 type="video/mp4">
+           </video>`
+    }
+    return  `<figure class="image_video">
+                    ${html_injected}
+                    <figcaption>
+                        <h2>${  title}</h2>
+                        <div >
+                            <span class="like">${ likes}</span> 
+                            <button class="like" type="button"  >
+                                <i class="fa-regular fa-heart"></i>
+                            </button> 
+                        </div>
+                    </figcaption>
+
+                </figure>`
+     
+    }
+return { photographerId, title,image,video,likes, templatePhotographerMedia  };
+
+ }
+
+
+ //-------------------------------------------------------
+ function photographerTemplate(data) {
+    const { name, portrait,city,country,tagline,price,id } = data;
+    console.log(data)
+    
+    const picture = `assets/photographersGallery/portrait/${portrait}`;
+
+    function getUserCardDOM() {
+        return article = `
+        <article  aria-label="profil de ${name}">
+			<a href="photographer.html?id=${id}"   id="profil- link-${id}" class="profil-link">
+			<div class="reflect">
+			<img src="${picture}" alt="Photo de ${name}" aria-label="Photo de ${name}"/>
+			</div>
+			<h2>${name}</h2>
+            </a>
+            <p class="location">${city}, ${country}</p>
+            <p class="tagline">${tagline}</p>
+            <p class="price">${price}€/jour</p>
+		</article> `;
+          }
+
+
+ function getPhotographerInfos () {
+            return article =   
+             `
+                <article aria-label="Photographer Profil" class="profil_photograph">
+                    <div class=' infos_photograph'>
+                        <h2>${ name}</h2>
+                        <p class=" city_photograph">${ city}, ${country}</p>
+                        <p class=" tagline_photograph">${ tagline}</p>
+                        
+                    </div>
+                    <button class="contact_button"   onclick="displayModal()">Contactez-moi</button>
+                    <a href='#' title=' '><img src="./assets/photographersGallery/portrait/${portrait}"  }"></a>
+                </article>
+                `
+      }
+        
+    return {  city, country, name, portrait, price, tagline, picture, getUserCardDOM, getPhotographerInfos  };
+  
+}
    
-
-  
-
-
-
-
-
-
-
-
-
-
-// //Mettre le code JavaScript lié à la page photographer.html
-
-
-// async function displayPhotographers() { 
-//     let url=(`./data/photographers.json `)
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     // let photographers= data.photographers;
-//     // console.log(data)
-
-//     // //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-//     //  function displayPhotographerProfil(data) {
-//         let photographersData = data.photographers;
-//         console.log(photographersData);
-//         // console.log(photographers);
-//         const id = window.location.search.split('id=')[1];
-//     console.log(id);
-//         const photographers = !id ? photographersData : photographersData.filter(photographer => photographer.id == id);
-//         console.log(photographers )
-//         console.log(photographers[0].name)
-//         console.log(photographers[0].city)
-//         // console.log(photographers[0].alt)
-//         console.log(photographers[0].portrait)
-        
-//         const alif= photographersData.filter(photographer => photographer.id );
-//         const baa= photographersData.filter(photographer => photographer.id == id);
-//         console.log(alif);
-//         console.log(baa);
-
-//        let nameForm=document.getElementById("name_form")
-//        nameForm.innerHTML=photographers[0].name
-//         // console.log(nameForm);
-      
-
-          
- 
-//        sectionPhotographerProfil.innerHTML = templatePhotographerProfil;
-//         // new Modal().modal(photographersData);
-//         // new Form().fields();
-//     }
-//     displayPhotographers()
-//     //zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-
-//     // let id=photographers.id
-//     // console.log(id)
-        
-//     //     console.log(photographers[1].id)
-//     //     console.log(photographers[0])
-//     //     console.log(photographers)
-  
-
-//     // let photographs_header= document.getElementsByClassName("photograph-header")
-    
-//     // for(let photograph_header of photographs_header){
-//     //     console.log(photograph_header)
-//     //     photograph_header.innerHTML+=`
-//     //     <a href=""><img src="" alt="photograph-header"></a>
-//     //     <p id="location">${photographers[0].country} ${photographers[0].city} </p>
-//     //     <p id="tagline">${photographers[0].tagline} </p>
-//     //     `
-//     // }
-        
-       
-        
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
